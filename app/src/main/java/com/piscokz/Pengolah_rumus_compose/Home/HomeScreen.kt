0b6a14dec6fdb031362f8eb1e3381d1e1c20e926
piscokz <fariyd001@gmail.com>
2024-06-19 15:38:56 +0700

    menampilkan program konversi ukuran panjang
    catatan :
    
    - program belum di tampilkan dikarenakan ada masalah pada logika konversi perhitungan koma.

--- app/src/main/java/com/piscokz/Pengolah_rumus_compose/Home/HomeScreen.kt
+++ app/src/main/java/com/piscokz/Pengolah_rumus_compose/Home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.piscokz.Pengolah_rumus_compose.Home
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -8,15 +9,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -28,18 +26,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.piscokz.Pengolah_rumus_compose.Programs.listProgram
-import com.piscokz.Pengolah_rumus_compose.Programs.listRumus
-import com.piscokz.Pengolah_rumus_compose.Programs.navProgram
-import com.piscokz.Pengolah_rumus_compose.Programs.switchButtonColors
 import com.piscokz.Pengolah_rumus_compose.Programs.switchColor
-import com.piscokz.Pengolah_rumus_compose.Programs.tipeProgram
 import com.piscokz.Pengolah_rumus_compose.R
-import com.piscokz.Pengolah_rumus_compose.Screen
 import com.piscokz.Pengolah_rumus_compose.ui.theme.PengolahRumusComposeTheme
 import java.util.Locale
 
@@ -69,7 +61,10 @@ fun Home(
                 }
 
             ) { paddingValues ->
-                HomeBody2(paddingValues = paddingValues, navController = navController)
+                HomeBody(
+                    paddingValues = paddingValues,
+                    navController = navController
+                )
             }
         }
     }
@@ -82,13 +77,15 @@ fun listProgram(
     screen_route: String,
     id_resource : Int
 ) {
+
     Surface(
         shape = RoundedCornerShape(15.dp),
+        modifier = Modifier.padding(bottom = 15.dp)
     ) {
         Row(
             modifier = Modifier
                 .clickable { navController.navigate(screen_route) }
-                .fillMaxWidth()
+                .widthIn(min = 500.dp, max = 700.dp)
                 .height(150.dp)
                 .padding(horizontal = 10.dp)
                 .background(MaterialTheme.colorScheme.surface),
@@ -112,7 +109,6 @@ fun listProgram(
                     )
             }
             Column(
-                Modifier.padding(end = 10.dp)
             ) {
                 Image(painter = painterResource(id = id_resource), contentDescription = null)
             }
@@ -121,7 +117,7 @@ fun listProgram(
 }
 
 @Composable
-fun HomeBody2(
+fun HomeBody(
     paddingValues: PaddingValues,
     navController: NavController
 ) {
@@ -167,147 +163,27 @@ fun HomeBody2(
                 screen_route = "lp_screen",
                 id_resource = R.drawable.rectangle_shape
             )
+            listProgram(
+                navController = navController,
+                namaProgram = "ukuran pajang",
+                jenisProgram = "konversi",
+                screen_route = "ukuran_panjang",
+                id_resource = R.drawable.calculate_img
+            )
         }
     }
 }
 
+
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "dark"
+)
+@Preview (
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "light"
+)
 @Composable
-fun HomeBody(
-    paddingValues: PaddingValues,
-    navController: NavController
-) {
-    val lebarTombol = 100.dp
-    val rekomendasi = listRumus[0]
-
-    LazyColumn {
-        item {
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues = paddingValues)
-                    .padding(start = 10.dp, bottom = 5.dp, top = 20.dp)
-
-            ) {
-                Text(
-                    color = switchColor(),
-                    text = "Rekomendasi ",
-                    style = MaterialTheme.typography.headlineMedium,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
-
-            Surface(
-                modifier = Modifier
-                    .padding(vertical = 10.dp, horizontal = 10.dp),
-                shape = RoundedCornerShape(15.dp)
-
-            ) {
-
-                Row(
-                    modifier = Modifier
-                        .padding(vertical = 50.dp, horizontal = 15.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-
-                    ) {
-                    Column(modifier = Modifier.weight(0.5f)) {
-                        Text(
-                            color = switchColor(),
-                            text = tipeProgram(rekomendasi),
-                            style = MaterialTheme.typography.bodyMedium,
-                            fontWeight = FontWeight.Medium
-
-                        )
-                        Text(
-                            color = switchColor(),
-                            text = rekomendasi,
-                            style = MaterialTheme.typography.headlineMedium,
-                            fontFamily = FontFamily.Default,
-                            fontWeight = FontWeight.Medium
-                        )
-                    }
-                    ElevatedButton(
-                        shape = RoundedCornerShape(35.dp),
-                        colors = switchButtonColors(),
-                        onClick = {
-                            navProgram(navController, rekomendasi)
-                        },
-                        modifier = Modifier
-                            .width(lebarTombol)
-                            .padding(start = 5.dp)
-                    ) {
-                        Text(
-                            color = switchColor(),
-                            text = "Lihat",
-                        )
-                    }
-                }
-            }
-            Column(
-                modifier = Modifier
-                    .padding(start = 10.dp, bottom = 5.dp, top = 20.dp)
-
-            ) {
-                Text(
-                    color = switchColor(),
-                    text = "Lainnya ",
-                    style = MaterialTheme.typography.headlineMedium,
-                    fontWeight = FontWeight.Bold,
-                )
-            }
-        }
-
-        items(listProgram.size) {
-            Row(
-                modifier = Modifier
-                    .padding(vertical = 15.dp, horizontal = 10.dp)
-            ) {
-                Surface(
-                    shape = RoundedCornerShape(15.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(horizontal = 10.dp, vertical = 10.dp)
-                            .fillMaxWidth()
-                            .requiredHeight(100.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Column(
-                            modifier = Modifier
-                                .weight(0.5f),
-                        ) {
-                            Text(
-                                color = switchColor(),
-                                text = tipeProgram(listProgram[it]),
-                                style = MaterialTheme.typography.bodySmall,
-                                fontWeight = FontWeight.Medium
-                            )
-                            Text(
-                                color = switchColor(),
-
-                                text = listProgram[it],
-                                style = MaterialTheme.typography.headlineSmall,
-                                overflow = TextOverflow.Ellipsis,
-                                maxLines = 1,
-                                fontWeight = FontWeight.Medium
-                            )
-                        }
-                        ElevatedButton(
-                            shape = RoundedCornerShape(35.dp),
-                            colors = switchButtonColors(),
-                            modifier = Modifier
-                                .width(lebarTombol),
-                            onClick = {
-                                navProgram(navController, listProgram[it])
-                            },
-                        ) {
-                            Text(
-                                text = "Lihat",
-                                textAlign = TextAlign.Center
-                            )
-                        }
-                    }
-                }
-            }
-        }
-
-    }
+private fun prevHomeScreen() {
+//    HomeBody(paddingValues = PaddingValues())
 }
